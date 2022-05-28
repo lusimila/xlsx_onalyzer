018bf4c8778aa8a596d2ed0c30edef619c407be7
lusimila <melodia.mila@mail.ru>
2022-05-28 13:13:28 +0300

    add docstring

--- settings.py
+++ settings.py
@@ -3,9 +3,11 @@
 
 from yaml import safe_load
 
+
 def load_config() -> dict:
     with open('config.yaml', 'r', encoding='utf-8') as file:
         result = safe_load(file)
         return result
+
 
 CONFIG = load_config()
